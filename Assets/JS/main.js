a4665657706d91405225cdae3582e134c22714aa
Eduardo Miguel <ed.mi.goveia@gmail.com>
2023-04-07 16:36:29 -0300

    Construção do menu e hero - Verificado

--- Assets/JS/main.js
+++ Assets/JS/main.js
@@ -1,1 +1,33 @@
-const navMneu = docu
+const navMenu = document.getElementById('nav-menu'),
+      navToggle =  document.getElementById('nav-toggle'),
+      navClose = document.getElementById('nav-close')
+
+/* Tornar menu visivel  */
+
+/* Validação */ 
+
+if(navToggle){
+    navToggle.addEventListener('click', () =>{
+        navMenu.classList.add('show-menu')
+    })
+}
+
+/* Esconder menu */
+
+if(navClose){
+    navClose.addEventListener('click', () =>{
+        navMenu.classList.remove('show-menu')
+    })
+}
+
+/* Remover menu mobile */
+
+const navLink = document.querySelectorAll('.nav__link')
+
+function linkAction(){
+    const navMenu = document.getElementById('nav-menu')
+    /* Ação: ao clicar em um dos itens do menu, ele volta a ser não visivel */
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
